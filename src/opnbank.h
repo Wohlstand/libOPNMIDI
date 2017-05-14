0265322747940ecd72d2f4cdcc99698d7c25c2f4
Wohlstand <admin@wohlnet.ru>
2017-05-14 05:02:27 +0300

    Fixed license intro (which was referred ADLMIDI, but it's another library)

--- src/opnbank.h
+++ src/opnbank.h
@@ -1,7 +1,7 @@
 /*
- * libADLMIDI is a free MIDI to WAV conversion library with OPL3 emulation
+ * libOPNMIDI is a free MIDI to WAV conversion library with OPN2 (YM2612) emulation
  *
- * Original ADLMIDI code: Copyright (c) 2010-2014 Joel Yliluoma <bisqwit@iki.fi>
+ * MIDI parser and player (Original code from ADLMIDI): Copyright (c) 2010-2014 Joel Yliluoma <bisqwit@iki.fi>
  * ADLMIDI Library API:   Copyright (c) 2016 Vitaly Novichkov <admin@wohlnet.ru>
  *
  * Library is based on the ADLMIDI, a MIDI player for Linux and Windows with OPL3 emulation:
