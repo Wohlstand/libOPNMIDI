ddea5842535fa942e08ea5089c1473cc38372300
Wohlstand <admin@wohlnet.ru>
2017-11-18 21:56:58 +0300

    Clean-up: remove files are no more useful after CMake support was added

--- android/Android.mk
+++ android/Android.mk
@@ -3,8 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := opnmidi_shared
 LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libOPNMIDI.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../src
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../src $(LOCAL_PATH)/../include
 TARGET_PLATFORM = android-10
 
 include $(PREBUILT_SHARED_LIBRARY)
-
--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -9,7 +9,7 @@ LOCAL_C_INCLUDES := \
 LOCAL_ARM_MODE   := arm
 LOCAL_CPP_FEATURES := exceptions
 LOCAL_LDLIBS     := -llog
-LOCAL_SRC_FILES := src/opnmidi.cpp src/Ym2612_Emu.cpp \
+LOCAL_SRC_FILES := src/opnmidi.cpp src/Ym2612_ChipEmu.cpp \
                    src/opnmidi_load.cpp src/opnmidi_midiplay.cpp \
                    src/opnmidi_opn2.cpp src/opnmidi_private.cpp \
                    src/opnmidi_xmi2mid.c src/opnmidi_mus2mid.c
