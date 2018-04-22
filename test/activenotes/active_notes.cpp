446db125714edecdd73a0dbcb42b2068faf6a424
Vitaly Novichkov <admin@wohlnet.ru>
2018-04-22 12:38:29 +0300

    Draft for unit test

---
+++ test/activenotes/active_notes.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
+#include <string>
+#include <vector>
+
+#include "opnmidi_private.hpp"
+
+TEST_CASE( "vectors can be sized and resized", "[PtrList]" )
+{
+    OPNMIDIplay::MIDIchannel midi_ch;
+    OPNMIDIplay::OpnChannel  opn_ch;
+
+}
