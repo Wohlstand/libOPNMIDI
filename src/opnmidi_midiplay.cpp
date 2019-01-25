71d0efeb6519f1c5fd45a4c50532d2a7f819a8b4
JP Cimalando <jpcima@users.noreply.github.com>
2019-01-25 08:13:40 +0100

    force note-on to do immediate note killing

--- src/opnmidi_midiplay.cpp
+++ src/opnmidi_midiplay.cpp
@@ -298,7 +298,7 @@ bool OPNMIDIplay::realTime_NoteOn(uint8_t channel, uint8_t note, uint8_t velocit
 
     if(static_cast<size_t>(channel) > m_midiChannels.size())
         channel = channel % 16;
-    noteOff(channel, note);
+    noteOff(channel, note, velocity != 0);
     // On Note on, Keyoff the note first, just in case keyoff
     // was omitted; this fixes Dance of sugar-plum fairy
     // by Microsoft. Now that we've done a Keyoff,
@@ -1621,7 +1621,7 @@ void OPNMIDIplay::updatePortamento(size_t midCh)
     m_midiChannels[midCh].portamentoRate = rate;
 }
 
-void OPNMIDIplay::noteOff(size_t midCh, uint8_t note)
+void OPNMIDIplay::noteOff(size_t midCh, uint8_t note, bool forceNow)
 {
     MIDIchannel &ch = m_midiChannels[midCh];
     MIDIchannel::notes_iterator i = ch.find_activenote(note);
@@ -1629,7 +1629,7 @@ void OPNMIDIplay::noteOff(size_t midCh, uint8_t note)
     if(!i.is_end())
     {
         MIDIchannel::NoteInfo &ni = i->value;
-        if(ni.ttl <= 0)
+        if(forceNow || ni.ttl <= 0)
             noteUpdate(midCh, i, Upd_Off);
         else
             ni.isOnExtendedLifeTime = true;
