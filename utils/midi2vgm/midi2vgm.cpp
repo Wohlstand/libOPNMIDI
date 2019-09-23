18a9c8775344d1a72355538d1400df7ef28aab1e
Wohlstand <admin@wohlnet.ru>
2019-09-23 19:51:15 +0300

    Midi2VGM: Fixed some Windows builds

--- utils/midi2vgm/midi2vgm.cpp
+++ utils/midi2vgm/midi2vgm.cpp
@@ -29,6 +29,36 @@
 #include <algorithm>
 #include <signal.h>
 
+#if defined(_MSC_VER) && _MSC_VER < 1900
+
+#define snprintf c99_snprintf
+#define vsnprintf c99_vsnprintf
+
+__inline int c99_vsnprintf(char *outBuf, size_t size, const char *format, va_list ap)
+{
+    int count = -1;
+
+    if (size != 0)
+        count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
+    if (count == -1)
+        count = _vscprintf(format, ap);
+
+    return count;
+}
+
+__inline int c99_snprintf(char *outBuf, size_t size, const char *format, ...)
+{
+    int count;
+    va_list ap;
+
+    va_start(ap, format);
+    count = c99_vsnprintf(outBuf, size, format, ap);
+    va_end(ap);
+
+    return count;
+}
+#endif
+
 #include <opnmidi.h>
 
 extern "C"
@@ -60,7 +90,7 @@ static void debugPrint(void * /*userdata*/, const char *fmt, ...)
     char buffer[4096];
     std::va_list args;
     va_start(args, fmt);
-    int rc = std::vsnprintf(buffer, sizeof(buffer), fmt, args);
+    int rc = vsnprintf(buffer, sizeof(buffer), fmt, args);
     va_end(args);
     if(rc > 0)
     {
