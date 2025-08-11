a7b6c83df8dc9abea10303ddd73542aeb1e6bf09
Wohlstand <admin@wohlnet.ru>
2025-08-11 14:08:12 +0300

    opn_wave_synth: turn samples into embedded heads

---
+++ test/pcm-samples/make_heads.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wopn2hpp kick.raw ../../src/chips/wavesynth/pcm_kick.h
+wopn2hpp snare.raw ../../src/chips/wavesynth/pcm_snare.h
+wopn2hpp snare2.raw ../../src/chips/wavesynth/pcm_snare2.h
+
+wopn2hpp drums1kick.raw ../../src/chips/wavesynth/pcm_drums1kick.h
+wopn2hpp drums3kick.raw ../../src/chips/wavesynth/pcm_drums3kick.h
+wopn2hpp drums3snare.raw ../../src/chips/wavesynth/pcm_drums3snare.h
+wopn2hpp drums1snare.raw  ../../src/chips/wavesynth/pcm_drums1snare.h
+
+wopn2hpp orchestraKik.raw ../../src/chips/wavesynth/pcm_orchestraKik.h
+wopn2hpp orchestraBassDrum.raw ../../src/chips/wavesynth/pcm_orchestraBassDrum.h
+wopn2hpp orchestraSnare.raw ../../src/chips/wavesynth/pcm_orchestraSnare.h
+
