12ed9df6d8380188ed3dfd61ab558f4c445ff2f0
Wohlstand <admin@wohlnet.ru>
2026-01-01 02:21:00 +0300

    Update copyright year for 2026

--- src/chips/pmdwin_opna.h
+++ src/chips/pmdwin_opna.h
@@ -1,7 +1,7 @@
 /*
  * Interfaces over Yamaha OPN2 (YM2612) chip emulators
  *
- * Copyright (C) 2025 Vitaly Novichkov (Wohlstand)
+ * Copyright (C) 2026 Vitaly Novichkov (Wohlstand)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- utils/vlc_codec/libopnmidi.c
+++ utils/vlc_codec/libopnmidi.c
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * libopnmidi.c: Software MIDI synthesizer using OPN2 Synth emulation
  *****************************************************************************
- * Copyright © 2017-2025 Vitaly Novichkov
+ * Copyright © 2017-2026 Vitaly Novichkov
  * $Id$
  *
  * This program is free software: you can redistribute it and/or modify
