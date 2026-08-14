3e6612377fbcf652056586b2562f00860c5bb0e7
Wohlstand <admin@wohlnet.ru>
2026-08-14 22:52:53 +0300

    Ouch

--- src/opnmidi.cpp
+++ src/opnmidi.cpp
@@ -405,7 +405,7 @@ OPNMIDI_EXPORT int opn2_getAutoArpeggio(OPN2_MIDIPlayer *device)
 
 OPNMIDI_EXPORT void opn2_setModeEMIDI(OPN2_MIDIPlayer *device, int emidiEn)
 {
-#ifndef ADLMIDI_DISABLE_MIDI_SEQUENCER
+#ifndef OPNMIDI_DISABLE_MIDI_SEQUENCER
     if(!device)
         return;
 
